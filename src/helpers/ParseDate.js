85609bae2a62f381cdff5d4ab2f1921778762776
BayelOsorov <osorov.bayel@gmail.com>
2022-02-24 20:24:15 +0600

    replace - to . in date

--- src/helpers/ParseDate.js
+++ src/helpers/ParseDate.js
@@ -2,9 +2,9 @@ export const parseDate = (UNIX_timestamp) => {
   let d = new Date(UNIX_timestamp * 1000);
   let datestring =
     ("0" + d.getDate()).slice(-2) +
-    "-" +
+    "." +
     ("0" + (d.getMonth() + 1)).slice(-2) +
-    "-" +
+    "." +
     d.getFullYear() +
     " " +
     ("0" + d.getHours()).slice(-2) +
